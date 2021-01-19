d65fe934b2a23651ffc6378506b31952866892c9
xuanhapham <phamthixuanha@secure.janeto.com>
2021-01-19 18:04:40 +0700

    #6 offer screen

--- food_ordering/lib/ui/screen/home_screen/widgets/home_tab_view/widgets/Offers_tab/widgets/restaurants_tab.dart
+++ food_ordering/lib/ui/screen/home_screen/widgets/home_tab_view/widgets/Offers_tab/widgets/restaurants_tab.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:intl/intl.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
 class RestaurantsTab extends StatefulWidget {
   RestaurantsTab({Key key}) : super(key: key);
@@ -27,11 +28,123 @@ class _RestaurantsTabState extends State<RestaurantsTab> {
           ),
           _buildTitle("Today's Offers", isShowViewAll: false),
           Container(
-            height: 300.h,
+            height: 158.h,
             child: ListView(
               scrollDirection: Axis.horizontal,
               children: <Widget>[
                 ...TODAY_OFFER_LIST.map((offer) => _buildTodayOffer(offer))
+              ],
+            ),
+          ),
+          _buildTitle(
+            "Free Delivery *",
+          ),
+          Container(
+            height: 230.h,
+            child: GridView.count(
+              childAspectRatio: 0.4,
+              scrollDirection: Axis.horizontal,
+              crossAxisCount: 2,
+              children: <Widget>[
+                ...FREE_DELIVERY_LIST.map((offer) => _buildFreeDelivery(offer))
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildFreeDelivery(Offer offer) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(6.w, 6.h, 13.w, 6.h),
+      margin: EdgeInsets.only(right: 10.w, bottom: 10.h),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(5.r),
+          border: Border.all(color: Colors.grey[200])),
+      child: Row(
+        children: [
+          Container(
+            height: double.infinity,
+            width: 80.w,
+            margin: EdgeInsets.symmetric(vertical: 4.h),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage(offer.imageUrl),
+              ),
+              borderRadius: BorderRadius.circular(5.r),
+            ),
+          ),
+          SizedBox(
+            width: 10.w,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  offer.title,
+                  overflow: TextOverflow.ellipsis,
+                  style:
+                      TextStyle(fontSize: 15.sp, fontWeight: FontWeight.w500),
+                ),
+                Text(
+                  offer.type,
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      fontSize: 12.sp,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w500),
+                ),
+                SizedBox(
+                  height: 10.h,
+                ),
+                Container(
+                  height: 1.h,
+                  width: 40.w,
+                  color: Colors.grey[300],
+                  margin: EdgeInsets.only(right: 5.w),
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      MaterialCommunityIcons.star,
+                      color: Colors.orangeAccent,
+                      size: 16.sp,
+                    ),
+                    Text(
+                      offer.star.toString(),
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          fontSize: 12.sp,
+                          color: Colors.grey,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    Expanded(
+                      child: Text(
+                        "  •  " + offer.description,
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                            fontSize: 12.sp,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 6.h,
+                ),
+                Text(
+                  '€' + offer.price.toStringAsFixed(2),
+                  style: TextStyle(
+                      color: Colors.grey[900],
+                      fontSize: 15.sp,
+                      fontWeight: FontWeight.w700),
+                ),
               ],
             ),
           ),
@@ -45,6 +158,7 @@ class _RestaurantsTabState extends State<RestaurantsTab> {
       width: 150.w,
       margin: EdgeInsets.only(right: 15.w),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Stack(
             alignment: Alignment.topRight,
@@ -77,9 +191,31 @@ class _RestaurantsTabState extends State<RestaurantsTab> {
               )
             ],
           ),
+          SizedBox(
+            height: 5.h,
+          ),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [Text("")],
+            children: [
+              Expanded(
+                child: Text(
+                  offer.title,
+                  style: TextStyle(
+                    fontSize: 15.sp,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              Icon(
+                MaterialCommunityIcons.star,
+                color: Colors.orangeAccent,
+              ),
+              Text(offer.star.toString())
+            ],
+          ),
+          Text(
+            offer.description,
+            textAlign: TextAlign.start,
+            style: TextStyle(color: Colors.grey, fontSize: 13.sp),
           )
         ],
       ),
@@ -88,7 +224,7 @@ class _RestaurantsTabState extends State<RestaurantsTab> {
 
   Widget _buildTitle(String title, {bool isShowViewAll = true}) {
     return Container(
-      margin: EdgeInsets.fromLTRB(0, 25.h, 0, 10.w),
+      margin: EdgeInsets.fromLTRB(0, 15.h, 10.w, 10.w),
       child: Row(
         children: <Widget>[
           Align(
@@ -222,20 +358,64 @@ class Offer {
   double star;
   String description;
   String imageUrl;
-  Offer(
-      {this.description,
-      this.discountPersent,
-      this.star,
-      this.title,
-      this.imageUrl});
+  double price;
+  String type;
+  Offer({
+    this.description,
+    this.discountPersent,
+    this.star,
+    this.title,
+    this.imageUrl,
+    this.type,
+    this.price,
+  });
 }
+
+List<Offer> FREE_DELIVERY_LIST = [
+  Offer(
+    description: "Only €30.0 for two",
+    discountPersent: 10,
+    star: 4.0,
+    title: "McDonald",
+    imageUrl: "assets/images/sample_image/pizza.jpg",
+    type: "Desserts",
+    price: 14.20,
+  ),
+  Offer(
+    description: "Only €23.0 for six",
+    discountPersent: 20,
+    star: 3.0,
+    title: "KFC",
+    imageUrl: "assets/images/sample_image/pasta.jpg",
+    type: "Desserts",
+    price: 14.20,
+  ),
+  Offer(
+    description: "Only €35.0 for two",
+    discountPersent: 15,
+    star: 2.0,
+    title: "Texact",
+    imageUrl: "assets/images/sample_image/steak.jpg",
+    type: "Desserts",
+    price: 14.20,
+  ),
+  Offer(
+    description: "Only €10.0 for two",
+    discountPersent: 50,
+    star: 1.0,
+    title: "Ruby",
+    imageUrl: "assets/images/sample_image/grilled_chicken.jpg",
+    type: "Desserts",
+    price: 14.20,
+  ),
+];
 
 List<Offer> TODAY_OFFER_LIST = [
   Offer(
     description: "Only €30.0 for two",
     discountPersent: 10,
     star: 4.0,
-    title: "McDonald",
+    title: "McDonald'S",
     imageUrl: "assets/images/sample_image/pizza.jpg",
   ),
   Offer(
